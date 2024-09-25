2d37a801027976fda7f999ed4aa6199716ba97d7
MagnusCracklsen2 <quiroz.frias.alvaro@gmail.com>
2024-09-24 18:17:55 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//CLASE24/09/2024
+//NOMBRE: QUIROZ FRIAS ALVARO
+
+
+//VARIABLES
+/* LAS VARIABLES GUARDAN INFORMACION QUE PUESES REUTILIZAR*/
+//dECLARACIONES DE VARIABLES
+let nombre = "Pedro";
+const PI = 3.1416;
+var edad = 25;
+
+//OPERADORES
+let a = 5;
+let b = 2;
+let suma = a + b; //7
+let resta = a - b; //3
+let multiplicacion = a * b; //10
+let division = a/b; //2.5
+
+//OPERADORES DE COMPARACION
+a > b; //True
+a < b; //False
+a == b; //False
+a === b; //False (compara)
